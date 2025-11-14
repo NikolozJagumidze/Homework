d780d4f803dc9a9d98d00d640c7bedb3f9402e62
Jugulioz <giog9153@gmail.com>
2025-11-14 17:41:15 +0400

    Add files via upload

---
+++ homework001.py
@@ -0,0 +1,12 @@
+print("Nikoloz Jagumidze")
+
+#String - ("100, 46, 79, 31")
+#Integer - (100, 74, 50, 84)
+#Boolean - True და False
+#Float - (45.3, 78.1, 53.8, -84.6)
+
+name = "Nikolozi"
+print(name)
+
+name = input("Nikolozi")
+print(name)
