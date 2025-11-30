81e65c2f9cf5cf40b8c07ff7e39def3355da25f5
Jugulioz <giog9153@gmail.com>
2025-11-30 16:39:27 +0400

    Add files via upload

---
+++ homework013.py
@@ -0,0 +1,23 @@
+#mutable მონაცემთა ტიპის შეცვლა შესაძლებელია
+#immutable მონაცემთა ტიპის შეცვლა შეუძლებელია
+
+
+family = ["Nikolozi", "Irina", "Giorgi", "Tiko"]
+family_age = ["14", "12", "41", "41"]
+
+print(family)
+print(family_age)
+
+
+Fruits = ["Banana", "Apple", "Orange","Cucumber"]
+print("I love" + " " + Fruits[1] + "s")
+
+
+Technics = ["TV", "Tablet", "Phone", "Computer", "Console"]
+print(Technics[1])
+print(Technics[3])
+print(Technics[-1])
+
+
+Sentence = ["GOA", "At", "I", "Study"]
+print(Sentence[2] + " " +Sentence[-1] + " " + Sentence[1] + " " + Sentence[0] + ".")
---
+++ homework015.py
@@ -0,0 +1,26 @@
+#სლაისინგით ჩვენ შეგვიძლია ავირჩიოთ სიაში მონიშნული სიტყვები ან სიტყვაში მონიშნული ასოები ინდექსინგის საშუალებით.
+
+
+cars = ["mercedes", "bmw", "audi", "nissan", "toyota", "opel"]
+
+print(cars[2:5])
+
+
+username=input("enter your name:")
+
+print(username[1:-1])
+
+
+name=input("Enter your name:")
+cars = ["mercedes", "bmw", "audi", "nissan", "toyota", "opel"]
+
+print(cars[0:3])
+print(name[3:])
+
+
+name1= input("enter your name:")
+
+if name1[0] == "რ":
+    print("Hello")
+else:
+    print("Bye")
